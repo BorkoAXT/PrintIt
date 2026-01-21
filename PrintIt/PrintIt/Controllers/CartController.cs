2a810424ddef61fd03cd36cf802f5aefd25085b6
Aleksandar-Stoyanov <aleks09sashko@gmail.com>
2026-01-21 21:18:09 +0200

    Added Cart page

---
+++ PrintIt/PrintIt/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrintIt.Data;
+using PrintIt.Models;
+using System.Security.Claims;
+
+namespace PrintIt.Controllers
+{
+    public class CartController : BaseController
+    {
+        public CartController(ApplicationDbContext context) : base(context) { }
+
+        [HttpGet("/Cart")]
+        public IActionResult Cart()
+        {
+            List<CartItem> userCart = new List<CartItem>();
+
+            // Check if user is logged in
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                // Take user ID from claims
+                string? userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (Guid.TryParse(userIdString, out Guid userId))
+                {
+                    // Get wishlist items for the user
+                    userCart = _context.ShopCarts
+                        .Include(c => c.Items)
+                        .ThenInclude(i => i.Print)
+                        .Where(c => c.UserId == userId)
+                        .SelectMany(c => c.Items).ToList();
+
+                    ViewData["CartItems"] = userCart;
+                }
+            }
+
+            ViewData["CartItems"] = userCart;
+
+            return View();
+        }
+    }
+}
